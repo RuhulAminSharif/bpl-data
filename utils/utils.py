4af6ae82181ff9a9c4b7871109e1b1543ad2804a
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2026-07-30 08:51:30 +0600

    feat: set up page config

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,13 @@
 from datetime import datetime
+import streamlit as st
+import base64
+import os
+
 
 def extract_series_slug(link: str) -> str:
     """Extract the series slug from a Cricinfo URL link."""
     return str(link).split("/")[-2]
+
 
 def parse_time_info(time_dict: dict) -> dict:
     """Parse time information from the match API response."""
@@ -17,3 +22,39 @@ def parse_time_info(time_dict: dict) -> dict:
         "lighting": time_dict.get("floodlit"),
         "session_schedule_raw": time_dict.get("hoursInfo"),
     }
+
+
+def get_base64_image(image_path):
+    if os.path.exists(image_path):
+        with open(image_path, "rb") as img_file:
+            return base64.b64encode(img_file.read()).decode()
+    return None
+
+
+def set_page_config():
+    # Set page config
+    st.set_page_config(
+        page_title="BPL Analysis Dashboard", page_icon="🏏", layout="wide"
+    )
+
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "images"))
+    logo_path = os.path.join(base_path, "bpl_logo.jpg")
+
+    encoded_image = get_base64_image(logo_path)
+
+    # Render Header
+    if encoded_image:
+        st.markdown(
+            f"""
+            <div style='text-align: center;'>
+                <img src='data:image/jpeg;base64,{encoded_image}' width='180' height='100' style='object-fit: contain;'/>
+                <h1 style='margin-top:10px;'>BPL Data Analysis Dashboard (2012-2026)</h1>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+    else:
+        st.markdown(
+            "<h1 style='text-align: center;'>BPL Data Analysis Dashboard (2012-2026)</h1>",
+            unsafe_allow_html=True,
+        )
