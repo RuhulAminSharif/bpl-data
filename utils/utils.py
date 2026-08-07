72ca5e2a1a5692b331708ab7c8d3e7ca7078aeb3
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2026-08-07 17:27:47 +0600

    feat: add series slug extraction and time info parsing functions

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,28 @@
 import base64
 import os
 import streamlit as st
+from datetime import datetime
+
+def extract_series_slug(link: str) -> str:
+    """Extract the series slug from a Cricinfo URL link."""
+    return str(link).split("/")[-2]
+
+    if not os.path.exists(folder):
+      os.makedirs(folder)
+      
+def parse_time_info(time_dict: dict) -> dict:
+    """Parse time information from the match API response."""
+    if not time_dict or "startTime" not in time_dict:
+        return {}
+
+    start_utc = datetime.fromisoformat(time_dict["startTime"])
+    return {
+        "match_date": start_utc.strftime("%Y-%m-%d"),
+        "match_start_utc": start_utc,
+        "scheduled_overs": time_dict.get("scheduledOvers"),
+        "lighting": time_dict.get("floodlit"),
+        "session_schedule_raw": time_dict.get("hoursInfo"),
+    }
 
 
 def set_page_config():
